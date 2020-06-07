17296c802fbc088c4366b7189f630c3ff8c09df9
Vitali Karpuk <karpuk-job@yandex.ru>
2020-06-07 16:49:13 +0300

    feat: add store

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './main.scss';
+// eslint-disable-next-line import/extensions
+import App from './app';
+// eslint-disable-next-line import/extensions
+import '@fortawesome/fontawesome-free/js/all.js';
+import store from './store';
+// eslint-disable-next-line import/order
+import { Provider } from 'react-redux';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'),
+);
